5a5192c517e43e419d596a11e884d6e1623b2fd8
JohnnySall <johnboy109@icloud.com>
2022-06-11 11:29:17 +0100

    Main Menu

--- JS/Tic Tac Toe/js/script.js
+++ JS/Tic Tac Toe/js/script.js
@@ -1,3 +1,33 @@
+const aiBtn = document.getElementById("aiBtn");
+const playerBtn = document.getElementById("playerBtn");
+
+aiBtn.addEventListener("click", function() {
+    gameSetup("ai");
+});
+playerBtn.addEventListener("click", function() {
+    gameSetup("player");
+});
+
+const p1DetailsContainer = document.getElementById("p1Container");
+const p2DetailsContainer = document.getElementById("p2Container");
+const playerAITitle = document.getElementById("playerAITitle");
+
+const gameSetup = (playerType) => {
+    if (playerType == "player"){
+        p1DetailsContainer.style.width = "40%";
+        p2DetailsContainer.style.width = "40%";
+        p2DetailsContainer.style.margin = "2.5%";
+        p2DetailsContainer.style.padding = "2.5%";
+        playerAITitle.innerText = "Player vs Player";
+    } else{
+        p1DetailsContainer.style.width = "90%";
+        p2DetailsContainer.style.width = "0%";
+        p2DetailsContainer.style.margin = "0%";
+        p2DetailsContainer.style.padding = "0%";
+        playerAITitle.innerText = "Player vs AI";
+    }
+};
+
 const gridItems = [];
 let x = 0;
 while (x < 9){
